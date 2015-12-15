9d8409c741a8cd6315abd97d70ab987ef23901f4
Jeremy Dittmer <jeremy.dittmer@qbotix.com>
2015-12-15 13:15:11 -0800

    Initial Commit

---
+++ Walmart_THA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Walmart_THA
+//
+//  Created by Jeremy Dittmer on 12/15/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Walmart_THA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Walmart_THA
+//
+//  Created by Jeremy Dittmer on 12/15/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
